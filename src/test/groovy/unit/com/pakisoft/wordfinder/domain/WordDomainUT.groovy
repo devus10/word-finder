e2d63c14c3db9cd65669e23cb0c98d7a22491e44
unknown <devus10@gmail.com>
2020-04-25 01:23:27 +0200

    test refacotred

--- src/test/groovy/unit/com/pakisoft/wordfinder/domain/WordDomainUT.groovy
+++ src/test/groovy/unit/com/pakisoft/wordfinder/domain/WordDomainUT.groovy
@@ -2,12 +2,14 @@ package com.pakisoft.wordfinder.domain
 
 import com.pakisoft.wordfinder.domain.word.Word
 import com.pakisoft.wordfinder.domain.word.WordDomainService
+import spock.lang.Unroll
 
 class WordDomainUT extends DomainSpecification {
 
     def wordService = new WordDomainService(dictionaryRepository())
 
-    def "for given string should find the word in dictionary with all possibles assembled words"() {
+    @Unroll
+    def "the '#inputString' string #existsInDictionaryString in dictionary and has #found.stringDictionaryAnagrams anagrams"() {
         expect:
         found == wordService.find(inputString, 'en')
 
@@ -15,11 +17,11 @@ class WordDomainUT extends DomainSpecification {
         found.existsInDictionary == existsInDictionary
 
         where:
-        inputString | found                                                | existsInDictionary
-        'car'       | Word.create('car', ['car', 'acr'] as Set)            | true
-        'Rome'      | Word.create('Rome', ['more', 'rome', 'Rome'] as Set) | true
-        'rome'      | Word.create('rome', ['more', 'rome', 'Rome'] as Set) | true
-        'oobl'      | Word.create('oobl', ['bool'] as Set)                 | false
-        'qwe'       | Word.create('qwe', [] as Set)                        | false
+        inputString | found                                                | existsInDictionary | existsInDictionaryString
+        'car'       | Word.create('car', ['car', 'acr'] as Set)            | true               | 'exists'
+        'Rome'      | Word.create('Rome', ['more', 'rome', 'Rome'] as Set) | true               | 'exists'
+        'rome'      | Word.create('rome', ['more', 'rome', 'Rome'] as Set) | true               | 'exists'
+        'oobl'      | Word.create('oobl', ['bool'] as Set)                 | false              | 'does not exist'
+        'qwe'       | Word.create('qwe', [] as Set)                        | false              | 'does not exist'
     }
 }
